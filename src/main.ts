2c5e127c633287175df8b829f6915b159894a19d
kminbo <kminbo214@gmail.com>
2025-01-06 20:43:08 +0900

    app.use

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const cors = require('cors');
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors({
+  app.enableCors({
     origin: true,
     credentials: true,
-    optionsSuccessStatus: 200,
-  }));
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS'
+  });
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
