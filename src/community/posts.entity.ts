e7f771964d7668c4f13f3cc434d481487531348a
kminbo <kminbo214@gmail.com>
2025-01-07 00:51:19 +0900

    nullable true 추가

--- src/community/posts.entity.ts
+++ src/community/posts.entity.ts
@@ -11,24 +11,24 @@ export class Posts{
     title: string;
 
     //이것도 토큰 설정 이후에 수정해야함! 유저 아이디 자동으로 들어가도록.
-    @Column()
+    @Column({nullable: true})
     user_id: string;
 
     @CreateDateColumn()
     created_at: Date;
 
-    @Column()
+    @Column({nullable: true})
     content: string;
 
-    @Column()
+    @Column({nullable: true})
     photos: string[];
 
-    @Column()
+    @Column({nullable: true})
     tags: string[];
 
     @Column()
     category:string; // chats or infos or comments
 
-    @OneToMany(()=> Reply, reply => reply.posts, {eager: true, cascade: true})
+    @OneToMany(()=> Reply, reply => reply.posts, {eager: true, cascade: true, nullable: true})
     replies: Reply[];
 }
