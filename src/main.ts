181c4d170fec7e92592d70b802fbeb295d2b84c5
kminbo <kminbo214@gmail.com>
2025-01-06 18:09:56 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use(cors());
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
