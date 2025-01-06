56748b2bcce1a7c9438414e4d61982dc2685480e
kminbo <kminbo214@gmail.com>
2025-01-06 18:07:23 +0900

    enableCors()

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:5173', //svelte 개발 서버 주소
-      'http://ticketpicker.co.kr'
-    ],
-    methods: 'GET, POST, PATCH, PUT, DELETE',
-    credentials: true,
-  })
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
